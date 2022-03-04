09002952e3ed18d96b80a72eabb3b8bb48030bea
Ivan Kyuchukov <ivan.kyuchukov@accedia.com>
2022-03-04 11:52:47 +0200

    for loops removed from contract; frontend not working

--- contracts/TechnoLimeStore.sol
+++ contracts/TechnoLimeStore.sol
@@ -5,167 +5,91 @@ import "./Ownable.sol";
 
 contract TechnoLimeStore is Ownable {
 
-  struct Product {
-    uint id;
-    string name;
-    uint price;
-    uint8 quantity;
-  }
-
   struct Order {
-    uint productId;
     uint8 productQuantity;
     address buyerAddress;
     uint createdAtBlock;
   }
+
+  struct Product {
+    string name;
+    uint price;
+    uint8 quantity;
+    mapping(address => Order) orders;
+  }
   
-  mapping(uint => Product) private products;
-  uint[] private productIds;
-  Order[] private orders;
+  mapping(string => Product) private products;
 
   uint private ALLOWED_RETURN_BLOCK_COUNT = 100;
-  uint private nextProductId = 1;
 
   enum ProductActions { Created, Updated }
   enum OrderActions { Created, Returned }
 
-  event ProductAction(uint productId, ProductActions action);
-  event OrderAction(uint productId, uint quantity, address buyerAddress, OrderActions action);
+  event ProductAction(string productName, ProductActions action);
+  event OrderAction(string productName, uint quantity, address buyerAddress, OrderActions action);
 
-  // Admin functions
   // We assume the name will be unique in our store (something like an SKU code)
   function createProduct(string calldata _name, uint _price, uint8 _quantity) public onlyOwner {
     require(bytes(_name).length > 0, "Name cannot be empty.");
     require(_price > 0, "Product cannot be free.");
-    require(_quantity > 0, "Please provide at least one product quantity.");
+    require(_quantity > 0, "Quantity cannot be zero.");
 
-    // TODO: trim name left and right here as well in the front-end
-    uint productId = productIdByName(_name);
+    bool productExists = bytes(products[_name].name).length > 0 ? true : false;
 
-    if (productId > 0) {
-      require(_quantity > products[productId].quantity, "Product exists. You can only increase its quantity.");
+    if (productExists) {
+      require(_quantity > products[_name].quantity, "Product exists. You can only increase its quantity.");
 
-      products[productId].quantity = _quantity;
+      products[_name].quantity = _quantity;
       
-      emit ProductAction(productId, ProductActions.Updated);
+      emit ProductAction(_name, ProductActions.Updated);
     }
     else {
-      productId = nextProductId;
-      products[productId].id = productId;
-      products[productId].name = _name;
-      products[productId].price = _price;
-      products[productId].quantity = _quantity;
+      products[_name].name = _name;
+      products[_name].price = _price;
+      products[_name].quantity = _quantity;
 
-      productIds.push(productId);
-      nextProductId++;
-
-      emit ProductAction(productId, ProductActions.Created);
+      emit ProductAction(_name, ProductActions.Created);
     }
   }
 
-  // Buyer functions
   // We asume a buyer can buy more than one product
-  // order IDs not used for simplicity
-  function placeOrder(uint _productId, uint8 _quantity) external payable {
-    Product storage product = products[_productId];
-    require(product.id > 0, "Product doesn't exist.");
-    int existingOrderIndex = getOrderIndex(_productId, msg.sender);
+  function placeOrder(string calldata _productName, uint8 _quantity) external payable {
+    Product storage product = products[_productName];
     require(_quantity > 0, "You need to buy at least one product.");
-    require(existingOrderIndex == -1, "Order exists. You cannot place more than one order for the same product.");
+    require(bytes(product.name).length > 0, "Product doesn't exist.");
+    require(product.orders[msg.sender].buyerAddress == address(0), "Order exists. You cannot place more than one order for the same product.");
     require(product.quantity >= _quantity, "Unsufficient product quantity in stock.");
     require(msg.value == product.price * _quantity, "Please send the exact price amount.");
 
     product.quantity -= _quantity;
-    orders.push(Order(_productId, _quantity, msg.sender, block.number));
+    product.orders[msg.sender] = Order(_quantity, msg.sender, block.number);
 
-    emit OrderAction(_productId, _quantity, msg.sender, OrderActions.Created);
+    emit OrderAction(_productName, _quantity, msg.sender, OrderActions.Created);
   }
 
   // We assume buyer cannot partially return an order
-  function returnOrder(uint _productId) external payable {
-    Product storage product = products[_productId];
-    require(product.id > 0, "Product doesn't exist.");
-    int existingOrderIndex = getOrderIndex(_productId, msg.sender);
-    require(existingOrderIndex > -1, "The order is not made by you.");
-    Order memory existingOrder = orders[uint(existingOrderIndex)];
+  function returnOrder(string calldata _productName) external payable {
+    Product storage product = products[_productName];
+    require(bytes(product.name).length > 0, "Product doesn't exist.");
+    require(product.orders[msg.sender].buyerAddress == msg.sender, "You have no orders for this product.");
+    Order memory existingOrder = product.orders[msg.sender];
     require(block.number - existingOrder.createdAtBlock <= ALLOWED_RETURN_BLOCK_COUNT, "You can only return an order no later than 100 blocks of time.");
 
+    uint8 existingOrderProdQuantity = existingOrder.productQuantity;
     product.quantity += existingOrder.productQuantity;
     uint amountToReturn = existingOrder.productQuantity * product.price;
     (bool sendSuccess, ) = address(msg.sender).call{value: amountToReturn}("");
     require(sendSuccess, "Failed to send refund.");
-    removeOrderAtIndex(uint(existingOrderIndex));
+    delete product.orders[msg.sender];
 
-    emit OrderAction(_productId, existingOrder.productQuantity, msg.sender, OrderActions.Returned);
+    emit OrderAction(product.name, existingOrderProdQuantity, msg.sender, OrderActions.Returned);
   }
 
-  // Helper functions
   function getStoreBalance() external view returns (uint) {
-        return address(this).balance;
+    return address(this).balance;
   }
 
-  function getAllProducts() external view returns (Product[] memory) {
-    // Looping through arrays is not a good idea due to performance and gas optimization and should be done off-chain in a web2 DB
-    // However I've left it here to change it later on if there's time
-    Product[] memory productsArray = new Product[](productIds.length);
-    for (uint i = 0; i < productIds.length; i++) {
-      Product memory product = products[productIds[i]];
-      productsArray[i] = product;
-    }
-    return productsArray;
-  }
-
-  function getProductOrders(uint _productId) external view returns (Order[] memory) {
-    // Looping through arrays is not a good idea due to performance and gas optimization and should be done off-chain in a web2 DB
-    // However I've left it here to change it later on if there's time
-    uint numberOfMatches;
-    for(uint i = 0; i < orders.length; i++) {
-      if (orders[i].productId == _productId) {
-        numberOfMatches++;
-      }
-    }
-    Order[] memory productOrders = new Order[](numberOfMatches);
-    uint nextIndex;
-    for(uint i = 0; i < orders.length; i++) {
-      if (orders[i].productId == _productId) {
-        productOrders[nextIndex] = orders[i];
-        nextIndex++;
-      }
-    }
-    return productOrders;
-  }
-
-  function getProductCount() external view returns (uint) {
-    return productIds.length;
-  }
-
-  function productIdByName(string memory _name) private view returns (uint) {
-    if (productIds.length == 0) {
-      return 0;
-    }
-
-    for (uint i = 0; i < productIds.length; i++) {
-      if (keccak256(abi.encodePacked(products[productIds[i]].name)) == keccak256(abi.encodePacked(_name))) {
-        return productIds[i];
-      }
-    }
-    return 0;
-  }
-
-  function getOrderIndex(uint _productId, address buyerAddress) private view returns (int) {
-    for (uint i = 0; i < orders.length; i++) {
-      if (orders[i].productId == _productId && orders[i].buyerAddress == buyerAddress) {
-        return int(i);
-      }
-    }
-    return -1;
-  }
-
-  function removeOrderAtIndex(uint _index) private {
-    require(_index < orders.length, "Index out of bound.");
-    for(uint i = _index; i < orders.length - 1; i++) {
-      orders[i] = orders[i + 1];
-    }
-    orders.pop();
+  function getProductDetails(string calldata _productName) external view returns (string memory, uint, uint8) {
+    return (products[_productName].name, products[_productName].price, products[_productName].quantity);
   }
 }
--- test/technolime-store-test.js
+++ test/technolime-store-test.js
@@ -27,31 +27,20 @@ describe('TechnoLimeStore contract', () => {
       }
     });
 
-    it('Should return 0 as products count', async () => {
-      expect(await tls.getProductCount())
-        .to.equal(0);
-    });
-
     // Product 1
     it('Should create product1', async () => {
       expect(await tls.createProduct('product1', 100, 10))
         .to.emit(tls, 'ProductAction')
-        .withArgs(1, 0); // emit ProductAction.Created(0) event
-    });
-
-    it('Should return 1 as products count', async () => {
-      expect(await tls.getProductCount())
-        .to.equal(1);
+        .withArgs('product1', 0); // emit ProductAction.Created(0) event
     });
 
     it('Should update product1', async () => {
       expect(await tls.createProduct('product1', 100, 11))
         .to.emit(tls, 'ProductAction')
-        .withArgs(1, 1); // emit ProductAction.Updated(1) event
+        .withArgs('product1', 1); // emit ProductAction.Updated(1) event
     });
 
     it('Should not be able to decrease quantity', async () => {
-      // .to.be.revertedWith('You can only increase quantity.') doesn't work for some reason..
       try {
         await tls.createProduct('product1', 100, 9);
       }
@@ -61,7 +50,6 @@ describe('TechnoLimeStore contract', () => {
     });
 
     it('Should not be able to create product with empty name', async () => {
-      // .to.be.revertedWith('You can only increase quantity.') doesn't work for some reason..
       try {
         await tls.createProduct('', 100, 9);
       }
@@ -71,7 +59,6 @@ describe('TechnoLimeStore contract', () => {
     });
 
     it('Should not be able to create product with 0 price', async () => {
-      // .to.be.revertedWith('You can only increase quantity.') doesn't work for some reason..
       try {
         await tls.createProduct('product1', 0, 9);
       }
@@ -81,12 +68,11 @@ describe('TechnoLimeStore contract', () => {
     });
 
     it('Should not be able to create product with 0 quantity', async () => {
-      // .to.be.revertedWith('You can only increase quantity.') doesn't work for some reason..
       try {
         await tls.createProduct('product1', 100, 0);
       }
       catch (error) {
-        expect(error.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Please provide at least one product quantity.'");
+        expect(error.message).to.equal("VM Exception while processing transaction: reverted with reason string 'Quantity cannot be zero.'");
       }
     });
 
@@ -94,26 +80,7 @@ describe('TechnoLimeStore contract', () => {
     it('Should create product2', async () => {
       expect(await tls.createProduct('product2', 100, 10))
         .to.emit(tls, 'ProductAction')
-        .withArgs(2, 0); // emit ProductAction.Created(0) event
-    });
-
-    it('Should return 2 as products count', async () => {
-      expect(await tls.getProductCount())
-        .to.equal(2);
-    });
-
-    it('Should return array of 2 products', async () => {
-      const result = await tls.getAllProducts();
-      const products = result.map((item) => {
-        return {
-          id: item.id.toNumber(),
-          name: item.name,
-          price: item.price.toNumber(),
-          quantity: item.quantity
-        }
-      });
-      expect(products.length)
-        .to.equal(2);
+        .withArgs('product2', 0); // emit ProductAction.Created(0) event
     });
 
     it('Should be able to only increase quantity', async () => {
